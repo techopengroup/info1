6fd9bf0426e734ca75ad64cb819d4efd1e5f7a7a
techopengroup <130648038+techopengroup@users.noreply.github.com>
2023-05-11 18:14:11 +0530

    Add files via upload

---
+++ info1/src/infotest/Info.java
@@ -0,0 +1,11 @@
+package infotest;
+
+public class Info {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Infocareer");
+
+	}
+
+}
